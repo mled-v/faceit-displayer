de094e7a86ad4d0df37932dd31ee757bf971547f
matt <matthewtledford@gmail.com>
2024-04-01 19:48:52 -0500

    added winrate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 from templates import api_connection 
 from time import time
+from collections import defaultdict
+import asyncio
+import aiohttp
 
 app = Flask(__name__)
 
@@ -19,7 +22,47 @@ def add():
     #player id and rank from match data
     player_id_and_rank = api_connection.get_player_id_and_rank(match_data)
 
-    return render_template('match.html',player_id_and_rank = player_id_and_rank )
+    #stores the teams player id's
+    team1_player_ids = []
+    team2_player_ids = []
+
+    #grabs the teams player id's
+    for player_data in player_id_and_rank['team1_data'].values():
+        team1_player_ids.append(player_data[1])
+    
+    for player_data in player_id_and_rank['team2_data'].values():
+        team2_player_ids.append(player_data[1])
+
+    team1_data = []
+
+    def get_tasks(session):
+            tasks = []
+            for player in team1_player_ids:
+                tasks.append(asyncio.create_task(session.get('https://open.faceit.com/data/v4/players/' 
+                    + player + '/stats/cs2',
+                    headers={'Authorization': 'Bearer fde1c0a3-1e2c-4c2e-a982-8d851b6c43d9'}, ssl = False)))
+            return tasks
+
+    async def get_winrate():
+        async with aiohttp.ClientSession() as session:
+            tasks = get_tasks(session)
+            responses = await asyncio.gather(*tasks)
+            for response in responses:
+                team1_data.append(await response.json())
+
+    asyncio.run(get_winrate())
+
+    team1_winrates = []
+
+    for item in team1_data:
+        team1_winrates.append(item['lifetime']['Win Rate %'])
+
+
+
+
+
+    return render_template('match.html',player_id_and_rank = player_id_and_rank, 
+                           team1_player_ids = team1_player_ids, team1_winrates = team1_winrates, team1_data = team1_data )
 
 
 if __name__ == '__main__':
--- templates/api_connection.py
+++ templates/api_connection.py
@@ -24,6 +24,16 @@ def get_match_data(match_id):
 
     return json_data
 
+def get_player_data(player_id):
+    response = requests.get(
+    'https://open.faceit.com/data/v4/players/' + player_id + '/stats/cs2',
+    headers={'Authorization': 'Bearer fde1c0a3-1e2c-4c2e-a982-8d851b6c43d9'}
+    )
+
+    json_data = response.json()
+
+    return json_data
+
 #This function will parse get_match_data and return [Player_id and rank]
 def get_player_id_and_rank(match_id):
     json_data = get_match_data(match_id)
@@ -37,14 +47,14 @@ def get_player_id_and_rank(match_id):
     
     for i in range(len(team1)):
         display_name = team1[i]['nickname']
-        player_id =team1[i]['game_player_id']
+        player_id =team1[i]['player_id']
         skill_level = team1[i]['game_skill_level']
         avatar = team1[i]['avatar']
         team1_ids[f'Player{i}'] = [display_name, player_id, skill_level, avatar]
 
     for i in range(len(team2)):
         display_name = team2[i]['nickname']
-        player_id =team2[i]['game_player_id']
+        player_id =team2[i]['player_id']
         skill_level = team2[i]['game_skill_level']
         avatar = team2[i]['avatar']
         team2_ids[f'Player{i}'] = [display_name, player_id, skill_level, avatar]
@@ -55,80 +65,11 @@ def get_player_id_and_rank(match_id):
     loaded_data["team1_data"] = team1_ids
     loaded_data["team2_data"] = team2_ids
 
+    print(loaded_data['team1_data'])
+
     return loaded_data
 
-print(get_player_id_and_rank(1))
 
- 
-
-def team1_id(match_id):
-    
-    response = requests.get(
-    'https://open.faceit.com/data/v4/matches/' + match_id,
-    headers={'Authorization': 'Bearer fde1c0a3-1e2c-4c2e-a982-8d851b6c43d9'})
-    
-
-    json_data = response.json()
-
-    team1 = json_data['teams']['faction1']['roster']
-    team1_ids = {}
-    
-    for i in range(len(team1)):
-        display_name = team1[i]['nickname']
-        player_id =team1[i]['game_player_id']
-        team1_ids[display_name] = player_id
-
-    return team1_ids
-
-def team2_id(match_id):
-
-    response = requests.get(
-    'https://open.faceit.com/data/v4/matches/' + match_id,
-    headers={'Authorization': 'Bearer fde1c0a3-1e2c-4c2e-a982-8d851b6c43d9'})
-
-    json_data = response.json()
-
-    team2 = json_data['teams']['faction2']['roster']
-    team2_ids = {}
-
-    for i in range(len(team2)):
-        display_name =team2[i]['nickname']
-        player_id = team2[i]['game_player_id']
-        team2_ids[display_name] = player_id
-
-    return team2_ids
-
-def team1_nicknames(match_id):
-    response = requests.get(
-    'https://open.faceit.com/data/v4/matches/' + match_id,
-    headers={'Authorization': 'Bearer fde1c0a3-1e2c-4c2e-a982-8d851b6c43d9'})
-
-    json_data = response.json()
-
-    team1 = json_data['teams']['faction1']['roster']
-    team1_nicknames = []
-
-    for i in range(len(team1)):
-        display_name =team1[i]['nickname']
-        team1_nicknames.append(display_name)
-
-    return team1_nicknames
-
-def team2_nicknames(match_id):
-    response = requests.get(
-    'https://open.faceit.com/data/v4/matches/' + match_id,
-    headers={'Authorization': 'Bearer fde1c0a3-1e2c-4c2e-a982-8d851b6c43d9'})
-
-    json_data = response.json()
-
-    team2 = json_data['teams']['faction2']['roster']
-    team2_nicknames = []
-
-    for i in range(len(team2)):
-        display_name =team2[i]['nickname']
-        team2_nicknames.append(display_name)
-
-    return team2_nicknames
 
 
 def win_rate_overall(player_id):
@@ -140,6 +81,8 @@ def win_rate_overall(player_id):
 
     overall_winrate = json_data['lifetime']['Win Rate %']
     return overall_winrate
+
+print(win_rate_overall('3137eb0b-7901-4783-9480-daced3aad07d'))
 
     
 def win_rate_by_map(player_id):
